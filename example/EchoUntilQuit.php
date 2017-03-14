cd40e0a371e909b664b3dfad904a5ff55bcff885
Marco Perone <m.perone@mvlabs.it>
2017-03-14 09:04:41 +0100

    completed EchoUntilQuit

--- example/EchoUntilQuit.php
+++ example/EchoUntilQuit.php
@@ -4,16 +4,23 @@ declare(strict_types = 1);
 
 namespace Marcosh\EffectorExample;
 
+use Marcosh\Effector\Compose;
 use Marcosh\Effector\Conditional;
 use Marcosh\Effector\Effect\Input\Readline;
-use Marcosh\Effector\Effect\Output\Echo_;
+use Marcosh\Effector\Effect\Quit\Exit_;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $readOrQuit = Conditional::ifThenElse(
     function (string $prompt) {return $prompt === 'quit';},
-    new Echo_(),
+    new Exit_(),
     new Readline()
 );
 
-$readOrQuit('quit');
+$readUntilQuit = Compose::iterable((function () use ($readOrQuit) {
+    while (true) {
+        yield $readOrQuit;
+    }
+})());
+
+$readUntilQuit('Type "stop" to end the execution');
