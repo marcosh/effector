d9d85d0bdee7c1fb1d107cf67d013b4ba15556de
Marco Perone <m.perone@mvlabs.it>
2017-03-15 07:49:39 +0100

    test argv

--- test/Effect/Input/ArgvTest.php
+++ test/Effect/Input/ArgvTest.php
@@ -4,9 +4,17 @@ declare(strict_types = 1);
 
 namespace Marcosh\EffectorTest\Effect\Input;
 
+use Marcosh\Effector\Effect\Input\Argv;
 use PHPUnit\Framework\TestCase;
 
 final class ArgvTest extends TestCase
 {
+    public function testArgvReturnsServerArgs()
+    {
+        $_SERVER['argv'] = ['value'];
 
+        $argv = new Argv();
+
+        self::assertSame(['value'], $argv());
+    }
 }
