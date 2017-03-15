83b66e77cc4b28b1604f24859647c4fb6310ccb4
Marco Perone <m.perone@mvlabs.it>
2017-03-15 08:26:40 +0100

    readline test

--- test/Effect/Input/ReadlineTest.php
+++ test/Effect/Input/ReadlineTest.php
@@ -4,9 +4,30 @@ declare(strict_types = 1);
 
 namespace Marcosh\EffectorTest\Effect\Input;
 
+use Marcosh\Effector\Effect\Input\Readline;
+use phpmock\mockery\PHPMockery;
 use PHPUnit\Framework\TestCase;
 
 final class ReadlineTest extends TestCase
 {
+    public function setUp()
+    {
+        $this->readlineMock = PHPMockery::mock('Marcosh\Effector\Effect\Input', 'readline');
+    }
 
+    public function testReadline()
+    {
+        $this->readlineMock->with('input')->once()->andReturn('output');
+
+        $readline = new Readline();
+
+        $output = $readline('input');
+
+        self::assertSame('output', $output);
+    }
+
+    public function tearDown()
+    {
+        \Mockery::close();
+    }
 }
