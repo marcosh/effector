c58b713592c778cec74eca1569514cbd2d0ab7ac
Marco Perone <m.perone@mvlabs.it>
2017-03-19 20:03:09 +0100

    file_get_contents effect incomplete tests

---
+++ test/Effect/File/FileGetContentsTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Marcosh\EffectorTest\Effect\File;
+
+use Marcosh\Effector\Effect\File\FileGetContents;
+use PHPUnit\Framework\Error\Warning;
+use PHPUnit\Framework\TestCase;
+
+final class FileGetContentsTest extends TestCase
+{
+    public function testFileGetContentsRetrievesFileContent()
+    {
+        $fileGetContents = new FileGetContents();
+
+        self::assertSame('hello!', $fileGetContents(__DIR__ . '/fixture.txt'));
+    }
+
+    public function testFileGetContentsRetrievesFileContentWithIncludePath()
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testFileGetContentsRetrievesFileContentWithContext()
+    {
+        self::markTestIncomplete();
+    }
+
+    public function testFileGetContentsRetrievesFileContentWithOffset()
+    {
+        $fileGetContents = new FileGetContents();
+
+        self::assertSame('llo!', $fileGetContents(__DIR__ . '/fixture.txt', false, null, 2));
+    }
+
+    public function testFileGetContentsRetrievesFileContentWithMaxLength()
+    {
+        $fileGetContents = new FileGetContents();
+
+        self::assertSame('hell', $fileGetContents(__DIR__ . '/fixture.txt', false, null, 0, 4));
+    }
+
+    public function testFileGetContentsFailsInNoFileIsFound()
+    {
+        $this->expectException(Warning::class);
+        $this->expectExceptionMessage(
+            'file_get_contents(/app/test/Effect/File/not-existing-file.txt): ' .
+            'failed to open stream: No such file or directory'
+        );
+
+        $fileGetContents = new FileGetContents();
+
+        $fileGetContents(__DIR__ . '/not-existing-file.txt');
+    }
+}
