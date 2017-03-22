2eb7d13ba3e2986184f467eb38604599d41678f7
Marco Perone <m.perone@mvlabs.it>
2017-03-22 19:22:51 +0100

    improve multiple echo

--- example/MultipleEcho.php
+++ example/MultipleEcho.php
@@ -9,18 +9,23 @@ use Marcosh\Effector\Effect\Output\Echo_;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$echoer = function (string $string) {
-    return [
-        function () use ($string) {return $string;},
+$constant = function (string $string) {
+    return function () use ($string) {return $string;};
+};
+
+// this function return a function with signature () => ()
+$echoer = function (string $string) use ($constant) {
+    return Compose::pieces(
+        $constant($string),
         new Echo_()
-    ];
+    );
 };
 
 // This produces a side effect after the other
-// It is similar to an endThen functionality
+// It is similar to an andThen functionality
 $compose = Compose::pieces(
-    ... $echoer('ciao' . PHP_EOL),
-    ... $echoer('hello' . PHP_EOL)
+    $echoer('ciao' . PHP_EOL),
+    $echoer('hello' . PHP_EOL)
 );
 
 $compose();
