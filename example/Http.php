c583b3a8447765d6c3e4c4d635309e09b450a4d0
Marco Perone <m.perone@mvlabs.it>
2017-04-22 10:18:39 +0200

    functional php website

---
+++ example/Http.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Marcosh\EffectorExample;
+
+use Marcosh\Effector\Compose;
+use Marcosh\Effector\Effect\Http\EmitResponse;
+use Marcosh\Effector\Effect\Http\ReceiveRequest;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Zend\Diactoros\Response;
+
+/**
+ * You could use this as a web application.
+ *
+ * This is really a dumb application, but using PSR-7 middleware this could be easily
+ * become something more interesting.
+ *
+ * You could use this with `php -S localhost:8000 example/Http.php`
+ * and then browse to `localhost:8000` to check this is actually working.
+ *
+ * If you don't have php 7.1 installed in your machine, you could use docker with
+ * `docker run --rm -ti -p 8000:8000 -v "$(pwd):/app" php:7.1-cli php -S 0.0.0.0:8000 /app/example/Http.php`
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$websiteLogic = function (RequestInterface $request): ResponseInterface {
+    return new Response\TextResponse((string) $request->getUri());
+};
+
+$app = Compose::pieces(
+    new ReceiveRequest(),
+    $websiteLogic,
+    new EmitResponse()
+);
+
+$app();
